6173e92a575fee5526eea8ea1fe20e4e13f2ee40
Abdul Moiz <progrmoiz@gmail.com>
2018-08-15 22:21:16 +0500

    update lib to contain inline and block actions

--- lib.sh
+++ lib.sh
@@ -16,27 +16,31 @@ COL_MAGENTA=$ESC_SEQ"35m"
 COL_CYAN=$ESC_SEQ"36m"
 
 function ok() {
-    echo -e "${COL_GREEN} [ok]${COL_RESET} "$1
+    echo -e "${COL_GREEN}[ok]${COL_RESET} "$1
 }
 
 function bot() {
-    echo -e "\n${COL_GREEN} \[._.]/${COL_RESET} - "$1
+    echo -e "\n${COL_GREEN}\[._.]/${COL_RESET} - "$1
 }
 
 function running() {
-    echo -e "${COL_YELLOW}  ⇒ ${COL_RESET}"$1": "
+    echo -e "${COL_YELLOW} ⇒ ${COL_RESET}"$1""
 }
 
 function action() {
-    echo -en "${COL_YELLOW} [action]${COL_RESET} $1"
+    echo -e "${COL_YELLOW}[action]${COL_RESET} "$@
+}
+
+function iaction() {
+    echo -en "${COL_YELLOW}[action]${COL_RESET} "$@" ..."
 }
 
 function warn() {
-    echo -e "${COL_YELLOW} [warning]${COL_RESET} "$1
+    echo -e "${COL_YELLOW}[warning]${COL_RESET} "$1
 }
 
 function error() {
-    echo -e "${COL_RED} [error]${COL_RESET} "$1
+    echo -e "${COL_RED}[error]${COL_RESET} "$1
 }
 
 # ask [QUESTION] [RESPONSE_VAR]
@@ -45,11 +49,19 @@ function ask() {
   read -r $2
 }
 
-function status() {
+function sstatus() {
     if [[ $? != 0 ]]; then
         error
     else 
         ok
+    fi
+}
+
+function status() {
+    if [[ $? != 0 ]]; then
+        printf ' error\n'
+    else 
+        printf ' done\n'
     fi
 }
 
@@ -60,13 +72,13 @@ function sourceNVM(){
 
 function require_npm() {
     sourceNVM
-    nvm use node --lts
+    nvm use node --lts &> /dev/null
     running "npm $1"
     npm list -g --depth 0 | grep $1@ > /dev/null
     if [[ $? != 0 ]]; then
         action "npm install -g $1"
-        npm install -g "$1" &> /dev/null
-        status
+        npm install -g "$1"
+        sstatus
     fi
 }
 
@@ -74,17 +86,17 @@ function require_pip() {
     running "pip3 $1"
     if [[ $(pip3 list --local --format=columns | grep "$1" | head -1 | cut -d' ' -f1) != "$1" ]]; then
         action "pip3 install $1"
-        pip3 install "$1" &> /dev/null
-        status
+        pip3 install "$1"
+        sstatus
     fi
 }
 
 function require_apt() {
     running "apt $1"
     if [[ $(dpkg-query -W -f='${Status}' "$1" 2>/dev/null | grep -c "ok installed") -eq 0 ]]; then
-        action "sudo apt install -y $1"
-        sudo apt install -y $1 &> /dev/null
-        status
+        action "sudo apt install $1"
+        sudo apt install -y -qq $1
+        sstatus
     fi
 }
 
@@ -93,12 +105,12 @@ function require_code() {
     if [[ $(code --list-extensions &> /dev/null | grep -c $1) -eq 0 ]]; then
         action "code --install-extension $1"
         code --install-extension $1 &> /dev/null
-        status
+        sstatus
     fi
 }
 
 function apt_update() {
     action "sudo apt update"
-    sudo apt update &> /dev/null
-    status
+    sudo apt update -q
+    sstatus
 }
